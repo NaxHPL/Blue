938f71511ee3247382be50f5a8b7b105c8b89c19
Alex <stefflera@yahoo.com>
2023-06-14 22:07:51 -0700

    Forgot to add these files

---
+++ Blue/src/ec/IScreenRenderable.cs
@@ -0,0 +1,72 @@
+﻿using BlueFw.Math;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace BlueFw;
+
+internal interface IScreenRenderable {
+
+    /// <summary>
+    /// Defines if this renderable will be rendered.
+    /// </summary>
+    bool Active { get; }
+
+    /// <summary>
+    /// The render layer this renderable is drawn on.
+    /// </summary>
+    /// <remarks>
+    /// If you plan to change this value after this renderable is added to a scene,
+    /// you must call <see cref="Scene.RenderOrderDirty"/> after changing it.
+    /// </remarks>
+    int RenderLayer { get; }
+
+    /// <summary>
+    /// Defines when this renderable will be drawn within its render layer.
+    /// Larger depth values get rendered first.
+    /// </summary>
+    /// <remarks>
+    /// If you plan to change this value after this renderable is added to a scene,
+    /// you must call <see cref="Scene.RenderOrderDirty"/> after changing it.
+    /// </remarks>
+    float LayerDepth { get; }
+
+    /// <summary>
+    /// This renderable's material.
+    /// </summary>
+    Material Material { get; }
+
+    /// <summary>
+    /// The screen space bounds of this renderable.
+    /// </summary>
+    Rect Bounds { get; }
+
+    /// <summary>
+    /// Renders this renderable using the specified sprite batch.
+    /// </summary>
+    void Render(SpriteBatch spriteBatch);
+}
+
+internal class ScreenRenderableOrderComparer : IComparer<IScreenRenderable> {
+
+    public int Compare(IScreenRenderable a, IScreenRenderable b) {
+        int compareResult = a.RenderLayer.CompareTo(b.RenderLayer);
+        if (compareResult != 0) {
+            return compareResult;
+        }
+
+        compareResult = b.LayerDepth.CompareTo(a.LayerDepth);
+        if (compareResult != 0) {
+            return compareResult;
+        }
+
+        if (a.Material == b.Material) {
+            return 0;
+        }
+
+        if (a.Material == null) {
+            return -1;
+        }
+
+        return 1;
+    }
+}
---
+++ Blue/src/scene/SceneHandler.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+
+namespace BlueFw;
+
+internal abstract class SceneHandler<T> {
+
+    protected abstract IComparer<T> itemComparer { get; }
+
+    protected readonly FastList<T> items = new FastList<T>();
+
+    readonly HashSet<T> itemsSet = new HashSet<T>();
+    readonly HashSet<T> itemsPendingAdd = new HashSet<T>();
+    readonly HashSet<T> itemsPendingRemove = new HashSet<T>();
+
+    bool itemOrderDirty = true;
+
+    public bool Register(T item) {
+        if (item == null) {
+            return false;
+        }
+
+        if (itemsSet.Contains(item)) {
+            return false;
+        }
+
+        if (itemsPendingRemove.Remove(item)) {
+            return true;
+        }
+
+        return itemsPendingAdd.Add(item);
+    }
+
+    public bool Unregister(T item) {
+        if (item == null) {
+            return false;
+        }
+
+        if (!itemsSet.Contains(item)) {
+            return false;
+        }
+
+        if (itemsPendingAdd.Remove(item)) {
+            return true;
+        }
+
+        return itemsPendingRemove.Add(item);
+    }
+
+    public void FlagItemOrderDirty() {
+        itemOrderDirty = true;
+    }
+
+    void ApplyPendingChanges() {
+        if (itemsPendingAdd.Count > 0) {
+            foreach (T item in itemsPendingAdd) {
+                items.Add(item);
+                itemsSet.Add(item);
+            }
+
+            itemsPendingAdd.Clear();
+            itemOrderDirty = true;
+        }
+
+        if (itemsPendingRemove.Count > 0) {
+            foreach (T item in itemsPendingRemove) {
+                items.Remove(item);
+                itemsSet.Remove(item);
+            }
+
+            itemsPendingRemove.Clear();
+        }
+    }
+
+    void EnsureItemsSorted() {
+        if (!itemOrderDirty) {
+            return;
+        }
+
+        items.Sort(itemComparer);
+        itemOrderDirty = false;
+    }
+
+    protected void PrepareItemsForHandling() {
+        ApplyPendingChanges();
+        EnsureItemsSorted();
+    }
+}
