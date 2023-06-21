597df3fcdbe70e4b087b1a979176c1cc04068032
Alex <stefflera@yahoo.com>
2023-06-20 19:47:42 -0700

    Random extensions

---
+++ Blue/src/ext/RandomExt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BlueFw.Extensions;
+
+public static class RandomExt {
+
+    /// <summary>
+    /// Returns a random float that is within a specified range.
+    /// </summary>
+    public static float Next(this Random random, float min, float max) {
+        if (min >= max) {
+            throw new ArgumentException("The minimum value must be less than the maximum!", nameof(min));
+        }
+
+        return random.NextSingle() * (max - min) + min;
+    }
+
+    /// <summary>
+    /// Returns a random double that is within a specified range.
+    /// </summary>
+    public static double Next(this Random random, double min, double max) {
+        if (min >= max) {
+            throw new ArgumentException("The minimum value must be less than the maximum!", nameof(min));
+        }
+
+        return random.NextDouble() * (max - min) + min;
+    }
+}
