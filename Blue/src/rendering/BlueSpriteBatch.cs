f6394daafa0a4f7e71c6b3a07f3fb8746980161d
Alex <stefflera@yahoo.com>
2023-06-28 18:47:35 -0700

    oops, fixed a bug

--- Blue/src/rendering/BlueSpriteBatch.cs
+++ Blue/src/rendering/BlueSpriteBatch.cs
@@ -130,7 +130,7 @@ public class BlueSpriteBatch : SpriteBatch {
     }
 
     internal new void End() {
-        End();
+        base.End();
 
         CurrentMaterial = null;
         CurrentTransformMatrix = null;
